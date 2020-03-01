f578382e72da7a8fe1c4aa0420d44a6098f64f98
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2020-03-01 10:08:58 -0300

    Update MoveEvent.java

--- RAntiBot/src/me/rafaelauler/antibot/Main/MoveEvent.java
+++ RAntiBot/src/me/rafaelauler/antibot/Main/MoveEvent.java
@@ -2,10 +2,12 @@ package me.rafaelauler.antibot.Main;
 
 import java.util.ArrayList;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
@@ -20,32 +22,130 @@ public class MoveEvent implements Listener{
 	/* 25 */     plugin = main;
 	/*    */   }
 	public static ArrayList<String> move = new ArrayList();
-
+	
 	@EventHandler
 	public void Player(PlayerJoinEvent e) {
 		if (!move.contains(e.getPlayer().getName()) && !e.getPlayer().hasPermission("antibot.bypass")) {
-			move.add(e.getPlayer().getName());
+			
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
+					/*     */         {
+					public void run()
+
+					/*     */           {
+						if (!move.contains(e.getPlayer().getName())) {
+					/* 165 */             
+					e.getPlayer().sendMessage(Main.plugin.getConfig().getString("MessageRememberToSneak").replace("&", "§"));;
+					/*     */           }
+					/* 167 */         }}, 60);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
+					/*     */         {
+					public void run()
+
+					/*     */           {
+						if (!move.contains(e.getPlayer().getName())) {
+					/* 165 */            
+					e.getPlayer().sendMessage(Main.plugin.getConfig().getString("MessageRememberToSneak").replace("&", "§"));;
+					/*     */           }
+					/* 167 */         }}, 90);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()					
+					/*     */         {
+				
+					public void run()
+
+					/*     */           {
+						if (!move.contains(e.getPlayer().getName())) {
+					/* 165 */            
+					e.getPlayer().sendMessage(Main.plugin.getConfig().getString("MessageRememberToSneak").replace("&", "§"));;
+					/*     */           }
+					/* 167 */         }}, 120);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
+					/*     */         {
+					public void run()
+
+					/*     */           {
+						if (!move.contains(e.getPlayer().getName())) {
+					/* 165 */            
+					e.getPlayer().sendMessage(Main.plugin.getConfig().getString("MessageRememberToSneak").replace("&", "§"));;
+					/*     */           }
+					/* 167 */         }}, 150);
+					Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
+							/*     */         {
+							public void run()
+
+							/*     */           {
+								if (!move.contains(e.getPlayer().getName())) {
+							/* 165 */            
+							e.getPlayer().sendMessage(Main.plugin.getConfig().getString("MessageRememberToSneak").replace("&", "§"));;
+							/*     */           }
+							/* 167 */         }}, 180);
+					Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
+							/*     */         {
+							public void run()
+
+							/*     */           {
+								if (!move.contains(e.getPlayer().getName())) {
+							/* 165 */            
+							e.getPlayer().sendMessage(Main.plugin.getConfig().getString("MessageRememberToSneak").replace("&", "§"));;
+							/*     */           }
+							/* 167 */         }}, 210);
+					Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
+					/*     */         {
+					public void run()
+
+					/*     */           {
+						if (!move.contains(e.getPlayer().getName())) {
+					/* 165 */            
+					e.getPlayer().sendMessage(Main.plugin.getConfig().getString("MessageRememberToSneak").replace("&", "§"));;
+					/*     */           }
+					/* 167 */         }}, 240);
+					Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
+							/*     */         {
+							public void run()
+
+							/*     */           {
+								if (!move.contains(e.getPlayer().getName())) {
+							/* 165 */            
+							e.getPlayer().sendMessage(Main.plugin.getConfig().getString("MessageRememberToSneak").replace("&", "§"));;
+							/*     */           }
+							/* 167 */         }}, 270);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
+				/*     */         {
+					public void run()
+
+					/*     */           {
+						if (!move.contains(e.getPlayer().getName())) {
+							e.getPlayer().kickPlayer(Main.plugin.getConfig().getString("KickedBecauseNotSneak").replace("&", "§"));
+					/*     */           }
+					/* 167 */         }}, 300);
 		}
 	}
+
 @EventHandler
 public void onPlayerMove(PlayerToggleSneakEvent e) {
-	if (move.contains(e.getPlayer().getName()) && !e.getPlayer().hasPermission("antibot.bypass")) {
-		move.remove(e.getPlayer().getName());
+	if (!move.contains(e.getPlayer().getName()) && !e.getPlayer().hasPermission("antibot.bypass") ) {
+		move.add(e.getPlayer().getName());
+		
 	}
 }
 	@EventHandler
 	public void chat(AsyncPlayerChatEvent e) {
-		if (move.contains(e.getPlayer().getName()) && !e.getPlayer().hasPermission("antibot.bypass")) {
-			e.getPlayer().sendMessage(this.main.getConfig().getString("MustSneakToTalk").replace("&", "�"));
+		if (!move.contains(e.getPlayer().getName())  && !e.getPlayer().hasPermission("antibot.bypass")) {
+			e.getPlayer().sendMessage(this.main.getConfig().getString("MustSneakToTalk").replace("&", "§"));
 			e.setCancelled(true);
 }
 	}
 		@EventHandler
 		public void leave(PlayerQuitEvent e) {
-			if (move.contains(e.getPlayer().getName()) && !e.getPlayer().hasPermission("antibot.bypass")) {
-				move.remove(e.getPlayer());
+			if (!move.contains(e.getPlayer().getName()) && !e.getPlayer().hasPermission("antibot.bypass")) {
 				JoinListener.canplay.remove(e.getPlayer().getName());
 				
 	}
+		}
+			@EventHandler
+			public void leave(PlayerKickEvent e) {
+				if (!move.contains(e.getPlayer().getName()) && !e.getPlayer().hasPermission("antibot.bypass")) {
+					JoinListener.canplay.remove(e.getPlayer().getName());
+					
+		}
 }
 }
