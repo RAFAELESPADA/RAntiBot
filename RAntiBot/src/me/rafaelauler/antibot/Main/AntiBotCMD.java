07e19544ed252aa8b0b3233d141473c4a69dfc16
Ricardo N <88561540+RicardoNz@users.noreply.github.com>
2021-08-20 12:53:42 -0300

    Code Clean

--- RAntiBot/src/me/rafaelauler/antibot/Main/AntiBotCMD.java
+++ RAntiBot/src/me/rafaelauler/antibot/Main/AntiBotCMD.java
@@ -9,91 +9,84 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-
-
-
-
 public class AntiBotCMD implements CommandExecutor {
 
-	static Main plugin;
-/*     */   public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-/*  91 */     if (commandLabel.equalsIgnoreCase("rantibot"))
-/*     */     {
-	/*  93 */       if (args.length == 0)
-	/*     */       {
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (commandLabel.equalsIgnoreCase("rantibot"))
+		{
+			if (args.length == 0 || args[0].equalsIgnoreCase("help"))
+			{
+				sender.sendMessage(
+						ChatColor.DARK_AQUA + "§m-----------" + ChatColor.AQUA + " RAntiBot COMMANDS " + ChatColor.DARK_AQUA + ChatColor.STRIKETHROUGH + "-------------", 
+						ChatColor.DARK_AQUA + "§eCreated by zEnderX5_",
+						"",
+						ChatColor.DARK_AQUA + " - " + ChatColor.AQUA + "/rantibot " + ChatColor.GREEN + "help" + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Display this help page",
+						ChatColor.DARK_AQUA + " - " + ChatColor.AQUA + "/rantibot " + ChatColor.GREEN + "info" + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Display plugin info!",
+						ChatColor.DARK_AQUA + " - " + ChatColor.AQUA + "/rantibot " + ChatColor.GREEN + "reload" + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Reload config file!",
+						ChatColor.DARK_AQUA + " - " + ChatColor.AQUA + "/rantibot " + ChatColor.GREEN + "reset" + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Reset the AntiBot player data!");
+				return true;
+			}
 
-	/* 106 */         sender.sendMessage(ChatColor.DARK_AQUA + "§m-----------" + ChatColor.AQUA + " RAntiBot COMMANDS " + ChatColor.DARK_AQUA + ChatColor.STRIKETHROUGH + "-------------");
-	/* 107 */         sender.sendMessage(ChatColor.DARK_AQUA + "§eCreated by zEnderX5_");
-	/* 108 */         sender.sendMessage("");
-	/* 109 */         sender.sendMessage(ChatColor.DARK_AQUA + " - " + ChatColor.AQUA + "/rantibot" + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Display this help page");
-	/* 110 */         sender.sendMessage(ChatColor.DARK_AQUA + " - " + ChatColor.AQUA + "/rantibot " + ChatColor.GREEN + "info" + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Display plugin info!");
-	/* 111 */         sender.sendMessage(ChatColor.DARK_AQUA + " - " + ChatColor.AQUA + "/rantibot " + ChatColor.GREEN + "reload" + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Reload config file!");
-	                  sender.sendMessage(ChatColor.DARK_AQUA + " - " + ChatColor.AQUA + "/rantibot " + ChatColor.GREEN + "reset" + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Reset the AntiBot player data!");
-	/* 112 */         
-	
+			if (args[0].equalsIgnoreCase("info"))
+			{
+				sender.sendMessage(
+						"§2§lCREDITS §f§lAND §e§lINFORMATION",
+						"§6\u279C §cPlugin Name: §eRAntiBot",
+						"§6\u279C §cPlugin Version: §eBUILD-5",
+						"§6\u279C §cAuthor: §ezEnderX5_ , Rafael Auler",
+						"§6\u279C §cAuthor Channel: http://bit.ly/2kC345B",
+						"§6\u279C §cSpigot Profile: http://bit.ly/2j5qvnM",
+						"§cThanks for use this plugin i really appreaciate IT",
+						"§cIf you like it consider giving a §e§l\u2605\u2605\u2605\u2605\u2605 §cReview",
+						"§cPS: §eSubscribe to my channel and follow me on Spigot Thanks! §9§l=)");
+				
+				return true;
+			}
 
-	/* 102 */         return true;
-	/*     */       }
-/*  93 */       if (args[0].equalsIgnoreCase("info"))
-{
-	/* 27 */       sender.sendMessage("§2§lCREDITS §f§lAND §e§lINFORMATION");
-	/* 28 */       sender.sendMessage("§6\u279C §cPlugin Name: §eRAntiBot");
-	/* 29 */       sender.sendMessage("§6\u279C §cPlugin Version: §eBUILD-5");
-	/* 30 */       sender.sendMessage("§6\u279C §cAuthor: §ezEnderX5_ , Rafael Auler");
-	/* 31 */       sender.sendMessage("§6\u279C §cAuthor Channel: http://bit.ly/2kC345B");
-	/* 32 */       sender.sendMessage("§6\u279C §cSpigot Profile: http://bit.ly/2j5qvnM");
-	/* 34 */       sender.sendMessage("§cThanks for use this plugin i really appreaciate IT");
-	/* 35 */       sender.sendMessage("§cIf you like it consider giving a §e§l\u2605\u2605\u2605\u2605\u2605 §cReview");
-	/* 36 */       sender.sendMessage("§cPS: §eSubscribe to my channel and follow me on Spigot Thanks! §9§l=)");
-	 return true;
-		/*     */       }
-/*  93 */       if (args[0].equalsIgnoreCase("reload"))
-/*     */       {
+			if (args[0].equalsIgnoreCase("reload"))
+			{
 
-	if (!sender.hasPermission("rantibot.reload")) {
-		Player p = (Player)sender;
-		/* 31 */         p.sendMessage("§b[RAntiBot] §cYou dont have permission!");
-		/* 32 */         
-		/* 33 */         return true;
-		/*    */       }
-		/*    */       Player p = (Player)sender;
-		/* 36 */       Plugin plugin = p.getServer().getPluginManager().getPlugin("RAntiBot");
-		/* 37 */       plugin.reloadConfig();
-		/* 38 */       p.getServer().getPluginManager().disablePlugin(plugin);
-		/* 39 */       p.getServer().getPluginManager().enablePlugin(plugin);
-		/* 40 */        p.sendMessage("§b[RAntiBot] §eThe config has been sucefully reloaded!");
+				if (!sender.hasPermission("rantibot.reload")) 
+				{
+					Player p = (Player)sender;
+					p.sendMessage("§b[RAntiBot] §cYou dont have permission!");
+					return true;
+				}
+				Player p = (Player)sender;
+				Plugin plugin = p.getServer().getPluginManager().getPlugin("RAntiBot");
+				plugin.reloadConfig();
+				p.getServer().getPluginManager().disablePlugin(plugin);
+				p.getServer().getPluginManager().enablePlugin(plugin);
+				p.sendMessage("§b[RAntiBot] §eThe config has been sucefully reloaded!");
 
-/* 102 */         return true;
-/*     */       }
-/*  93 */       if (args[0].equalsIgnoreCase("reset"))
-/*     */       {
+				return true;
+			}
 
-	if (!sender.hasPermission("rantibot.reset")) {
-		Player p = (Player)sender;
-		/* 31 */         p.sendMessage("§b[RAntiBot] §cYou dont have permission!");
-		/* 32 */         
-		/* 33 */         return true;
-		/*    */       }
-	if (!JoinListener.canplay.isEmpty()) {
-		JoinListener.canplay.remove(String.valueOf(JoinListener.canplay.size()));
-		sender.sendMessage("§b[RAntiBot] §eYou reset all antibot data!");
-		sender.sendMessage("§b[RAntiBot] §eNow all players that dont have the");
-		sender.sendMessage("§b[RAntiBot] §ePermission antibot.bypass");
-		sender.sendMessage("§b[RAntiBot] §eMust be verified by the antibot again");
-		Bukkit.getConsoleSender().sendMessage("§b[RAntiBot] §eThe player data has been reseted by " + sender.getName() + "!");
-	} else {
-		sender.sendMessage("§b[RAntiBot] §cDont exists any data to remove!");
+			if (args[0].equalsIgnoreCase("reset"))
+			{
+
+				if (!sender.hasPermission("rantibot.reset")) 
+				{
+					Player p = (Player)sender;
+					p.sendMessage("§b[RAntiBot] §cYou dont have permission!");
+					return true;
+				}
+
+				if (!JoinListener.canplay.isEmpty()) {
+					JoinListener.canplay.clear();
+					sender.sendMessage(
+							"§b[RAntiBot] §eYou reset all antibot data!",
+							"§b[RAntiBot] §eNow all players that dont have the",
+							"§b[RAntiBot] §ePermission antibot.bypass",
+							"§b[RAntiBot] §eMust be verified by the antibot again");
+					Bukkit.getConsoleSender().sendMessage("§b[RAntiBot] §eThe player data has been reseted by " + sender.getName() + "!");
+				} else {
+					sender.sendMessage("§b[RAntiBot] §cDont exists any data to remove!");
+				}
+				return true;
+			}
+			return true;
+		}
+		return false;
 	}
-/* 102 */         return true;
-/*     */       }
-
-		
-
-/* 102 */         return true;
-/*     */       }
-return false;
-
-
-
-}
 }
--- RAntiBot/src/me/rafaelauler/antibot/Main/JoinListener.java
+++ RAntiBot/src/me/rafaelauler/antibot/Main/JoinListener.java
@@ -3,50 +3,36 @@ package me.rafaelauler.antibot.Main;
 import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
-
-
-
-
-
+import me.rafaelauler.antibot.Main.Main;
 
 public class JoinListener implements Listener {
-	/*    */   private Main main;
-	/*    */   static Main plugin;
-	/*    */   
-	/*    */   public JoinListener(Main main)
-	/*    */   {
-	/* 24 */     this.main = main;
-	/* 25 */     plugin = main;
-	/*    */   }
-	/*    */   
+  
 	public static ArrayList<String> canplay = new ArrayList();
 
-/*     */   @EventHandler
-/*     */   public void onFirstLogin(PlayerLoginEvent e) {
-/*     */   Player p = e.getPlayer();
-if (!canplay.contains(p.getName()) && !p.hasPermission("antibot.bypass"))
-{
-	e.setKickMessage(this.main.getConfig().getString("MessageKick").replace("&", "§"));
-	e.disallow(PlayerLoginEvent.Result.KICK_OTHER, (this.main.getConfig().getString("MessageKick").replace("&", "§")));
-canplay.add(p.getName());
-MoveEvent.move.remove(p.getName());
-Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable()
-/*     */         {
-public void run()
-
-/*     */           {
-	if (!JoinListener.canplay.isEmpty()) {
-/* 165 */             JoinListener.canplay.remove(String.valueOf(JoinListener.canplay.size()));
-Bukkit.getConsoleSender().sendMessage("§b[RAntiBot] §eThe player data has been reseted!");
-/*     */           }
-/* 167 */         }}, 20 * 60 * this.main.getConfig().getInt(("Interval")));
-/*     */       }
-}
+	@EventHandler
+	public void onFirstLogin(PlayerLoginEvent e) {
+		Player p = e.getPlayer();
+		if (!canplay.contains(p.getName()) && !p.hasPermission("antibot.bypass")) {
+			e.setKickMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("MessageKick")));
+			e.disallow(PlayerLoginEvent.Result.KICK_OTHER, ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("MessageKick")));
+			canplay.add(p.getName());
+			MoveEvent.move.remove(p.getName());
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
+				public void run() {
+					if (!JoinListener.canplay.isEmpty()) {
+						JoinListener.canplay.clear()
+						Bukkit.getConsoleSender().sendMessage("§b[RAntiBot] §eThe player data has been reseted!");
+					}
+				}
+			}, 20 * 60 * Main.getInstance().getConfig().getInt(("Interval")));
+		}
+	}
 }
 
 
